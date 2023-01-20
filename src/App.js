8934fa248b4ee3fb57c8a6ef7ac2e3c31581393f
İbrahim ÜZÜL <ibrahim.uzul@kron.com.tr>
2023-01-20 11:27:21 +0300

    Notes, List

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import NoteList from './Components/NotesList';
 
-const App =()=>{
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "First Note",
+      date: "15/04/2022"
+    },
+    {
+      id: nanoid(),
+      text: "Second Note",
+      date: "11/04/2022"
+    },
+    {
+      id: nanoid(),
+      text: "Third Note",
+      date: "14/04/2022"
+    }
+  ]);
+
   return (
     <div className='container'>
-      <NoteList></NoteList>
+      <NoteList notes={notes}></NoteList>
     </div>
   );
 }
