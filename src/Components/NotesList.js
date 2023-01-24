c1cb0f60ac325119d62d64e48e50820402123cbf
İbrahim ÜZÜL <ibrahim.uzul@kron.com.tr>
2023-01-24 16:33:46 +0300

    NoteList const to class

--- src/Components/NotesList.js
+++ src/Components/NotesList.js
@@ -1,18 +1,22 @@
 import Note from './Note';
 import AddNote from './AddNote';
 
-const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
-    return (
-        <div className="notes-list">
-            {notes.map((note) =>
-                <Note key={note.id} id={note.id} text={note.text} date={note.date}
-                    handleDeleteNote={handleDeleteNote} />
-            )}
-            <AddNote
-                handleAddNote={handleAddNote}
-            ></AddNote>
-        </div>
-    );
+import React, { Component } from 'react'
+
+class NotesList extends Component {
+    render() {
+        return (
+            <div className="notes-list">
+                {this.props.notes.map((note) =>
+                    <Note key={note.id} id={note.id} text={note.text} date={note.date}
+                        handleDeleteNote={this.props.handleDeleteNote} />
+                )}
+                <AddNote
+                    handleAddNote={this.props.handleAddNote}
+                ></AddNote>
+            </div>
+        )
+    }
 }
 
 export default NotesList;
