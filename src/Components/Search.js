1b47c8fdf93b078bc4ff980f00e80faf0fc9d6ef
İbrahim ÜZÜL <ibrahim.uzul@kron.com.tr>
2023-01-24 14:58:17 +0300

    const Search to class React.Component converted with handle search event

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { MdSearch } from "react-icons/md";
 
-const Search = ({ handleSearchNote }) => {
-    return <div className="search">
-        <MdSearch className="search-icons" size='1.3em'></MdSearch>
-        <input onChange={(event) => handleSearchNote(event.target.value)} type="text" placeholder="type to search..."></input>
-    </div>;
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div className="search">
+            <MdSearch className="search-icons" size='1.3em'></MdSearch>
+            <input onChange={(event) => this.props.handleSearchNote(event.target.value)} type="text" placeholder="type to search..."></input>
+        </div>;
+    }
 }
 
 export default Search;
